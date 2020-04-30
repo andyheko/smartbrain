ebe095b54011efb7a22111fb86fb94a190553105
Andy Ko <34779522+andyheko@users.noreply.github.com>
2020-04-30 20:20:31 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Navigation from './components/Navigation/Navigation.js'
+import Navigation from './components/Navigation/Navigation'
 import './App.css';
 
 class App extends Component {
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navigation = () => {
   return (
-    <nav>
-      <p>Sign Out</p>
+    <nav style={{display: 'flex', justifyContent: 'flex-start'}}>
+      <p className='f3 link dim black underline pa5 pointer'>Sign Out</p>
     </nav>
   )
 }
